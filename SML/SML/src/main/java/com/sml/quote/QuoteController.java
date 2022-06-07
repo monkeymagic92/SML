737641fa923b19d4a27b8c9aa611fe51565a3386
monkeymagic92 <ddw0099@naver.com>
2022-06-07 21:23:05 +0900

    노트북 -> 데스크탑 push

--- SML/SML/src/main/java/com/sml/quote/QuoteController.java
+++ SML/SML/src/main/java/com/sml/quote/QuoteController.java
@@ -27,7 +27,7 @@ public class QuoteController extends CommonController {
 	@RequestMapping(value = "/quote/quote_race", method = RequestMethod.GET)
 	public String coinRaceIndex(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		// 깃 연동 끝
+		// 깃 연동 끝123123
 
 		// (Race) 10:00am데이터 list
 		model.addAttribute("list", service.selectCoinRaceList());
